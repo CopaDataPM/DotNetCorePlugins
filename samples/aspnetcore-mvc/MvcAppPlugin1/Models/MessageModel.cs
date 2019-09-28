4533fe34b23165fdeec6ea9898f3c344c5c5dcaf
Nate McMaster <nate.mcmaster@gmail.com>
2019-09-27 21:21:36 -0700

    Attempt to reproduce #77

---
+++ samples/aspnetcore-mvc/MvcAppPlugin1/Models/MessageModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcAppPlugin1
+{
+    public class MessageModel
+    {
+        public string Message { get; set; }
+    }
+}
--- samples/aspnetcore-mvc/MvcAppPlugin1/MyPluginController.cs
+++ samples/aspnetcore-mvc/MvcAppPlugin1/MyPluginController.cs
@@ -4,6 +4,23 @@ namespace MvcAppPlugin1
 {
     public class MyPluginController : Controller
     {
-        public IActionResult Index() => View();
+        public IActionResult Index()
+        {
+            return View(new MessageModel
+            {
+                Message = "Hello from .NET Core Plugin 1!"
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind] MessageModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Thing", "Invalid message");
+            }
+            return View(model);
+        }
     }
 }
